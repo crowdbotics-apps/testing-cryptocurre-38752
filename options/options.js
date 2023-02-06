fa35c2a0156fb0b3f5e1ef9679d861277cb5e8f6
Crowdbotics <team@crowdbotics.com>
2023-02-06 19:47:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testing-cryptocurre-38752",
+  url: "https://testing-cryptocurre-38752.botics.co",
+  api: "https://testing-cryptocurre-38752.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
